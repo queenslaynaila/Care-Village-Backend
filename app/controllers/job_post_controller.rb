f74491741d8d5ce4f692dbe75166151999a41efb
Queenslay Jema <queenslay.naila@student.moringaschool.com>
2022-11-10 21:30:45 +0300

    add job post logic

--- app/controllers/job_post_controller.rb
+++ app/controllers/job_post_controller.rb
@@ -8,7 +8,23 @@ class JobPostController < ApplicationController
     get '/job/:id' do
         Job.find(params[:id]).to_json
     end
-    
+
+    post  '/jobs' do
+        job = Job.create(
+            headline: params[:headline],
+            date_posted: params[:date_posted],
+            expires:params[:expires],
+            care_needed:params[:care_needed],
+            schedule: params[:schedule],
+            location: params[:location],
+            majorrequirement: params[:majorrequirement],
+            sitter_id: params[:sitter_id],
+            client_id: params[:client_id],
+            status:params[:status])
+         job.to_json
+    end
+
+
 
 
 end
